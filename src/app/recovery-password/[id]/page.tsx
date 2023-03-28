0ef5f62d6446fa80a374870605280bc4a2af9f4b
jtiagosantos <tiagosan040@gmail.com>
2023-03-27 22:56:28 -0300

    feat: integrate form with react hook form and zod

--- src/app/recovery-password/[id]/page.tsx
+++ src/app/recovery-password/[id]/page.tsx
@@ -1,12 +1,36 @@
 'use client';
 
 import type { NextPage } from 'next';
-import { ChakraProvider, Text, Flex, FormControl, Button } from '@chakra-ui/react';
-
-import { Input } from './components/input.component';
-import { Label } from './components/label.component';
+import { 
+  ChakraProvider, 
+  Text, 
+  Flex, 
+  FormControl, 
+  Button, 
+  Input, 
+  FormErrorMessage,
+  FormLabel,
+} from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { theme } from '../../../styles/theme';
+
+import type { SubmitHandler } from 'react-hook-form';
+
+const recoveryPasswordSchema = z.object({
+  password: z
+    .string()
+    .min(1, { message: 'Campo obrigatório' })
+    .min(6, { message: 'Mínimo de 6 caracteres' }),
+  confirmPassword: z
+    .string()
+    .min(1, { message: 'Campo obrigatório' })
+    .min(6, { message: 'Mínimo de 6 caracteres' })
+})
+
+type RecoveryPasswordFormData = z.infer<typeof recoveryPasswordSchema>;
 
 interface RecoveryPasswordProps {
   params: {
@@ -15,6 +39,28 @@ interface RecoveryPasswordProps {
 }
 
 const RecoveryPasswordPage: NextPage<RecoveryPasswordProps> = ({ params }) => {
+  const { register, handleSubmit, setError, formState: { errors } } = useForm<
+    RecoveryPasswordFormData
+  >({
+    defaultValues: {
+      password: '',
+      confirmPassword: '',
+    },
+    resolver: zodResolver(recoveryPasswordSchema),
+  });
+
+  const handleUpdatePassword: SubmitHandler<RecoveryPasswordFormData> = async ({ 
+    password,
+    confirmPassword,
+  }) => {
+    if (password !== confirmPassword) {
+      setError('confirmPassword', { message: 'As senhas não coincidem' });
+      return;
+    }
+
+    console.log(password);
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Flex maxW="350px" flexDir="column" align="flex-start" justify="center" mx="auto" p="24px">
@@ -22,22 +68,58 @@ const RecoveryPasswordPage: NextPage<RecoveryPasswordProps> = ({ params }) => {
           Alteração de Senha
         </Text>
 
-        <FormControl mt="16px">
-          <Label htmlFor="password">Senha</Label>
+        <FormControl 
+          as="form" 
+          onSubmit={handleSubmit(handleUpdatePassword)} 
+          isInvalid={!!(errors.password || errors.confirmPassword)} 
+          mt="16px"
+        >
+          <FormLabel htmlFor="password" fontWeight="normal" mb="2px" color="#D4D4D8">Senha</FormLabel>
           <Input 
+            {...register('password')}
             type="password"
-            id="password" 
+            id="password"
             placeholder="Digite sua senha" 
+            bgColor="#27272A"
+            border="none"
+            _placeholder={{
+              color: '#71717A'
+            }}
+            _focus={{
+              outlineColor: '#5B21B6',
+              boxShadow: 'none',
+              border: 'none',
+            }}
+            isInvalid={!!errors.password}
           />
+          {errors.password?.message && (
+            <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+          )}
 
-          <Label htmlFor="confirm-password" mt="16px">Confirmar senha</Label>
+          <FormLabel htmlFor="confirmPassword" fontWeight="normal" mb="2px" color="#D4D4D8" mt="16px">Confirmar senha</FormLabel>
           <Input 
+            {...register('confirmPassword')}
             type="password"
-            id="confirm-password" 
+            id="confirmPassword" 
             placeholder="Confirme sua senha" 
+            bgColor="#27272A"
+            border="none"
+            _placeholder={{
+              color: '#71717A'
+            }}
+            _focus={{
+              outlineColor: '#5B21B6',
+              boxShadow: 'none',
+              border: 'none',
+            }}
+            isInvalid={!!errors.confirmPassword}
           />
+          {errors.confirmPassword?.message && (
+            <FormErrorMessage>{errors.confirmPassword.message}</FormErrorMessage>
+          )}
 
           <Button 
+            type="submit"
             w="full" 
             bgColor="#5B21B6" 
             mt="24px" 
